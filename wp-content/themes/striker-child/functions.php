b7f5dbd83c7c718c076c9d78d30b633cfb1bfb68
Raúl de Santos Rico <rasantos@it.uc3m.es>
2020-06-09 02:46:35 +0200

    fixed striker-child php code that wasn't working in a external host

--- wp-content/themes/striker-child/functions.php
+++ wp-content/themes/striker-child/functions.php
@@ -1,29 +1,16 @@
 <?php
-
-				//Striker Child Theme
-
-				add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
-
-				function my_theme_enqueue_styles() {
-					$parenthandle = 'style'; // This is 'style' for the Striker theme.
-					$theme = wp_get_theme();
-					
-					wp_enqueue_style( $parenthandle, get_template_directory_uri() . '/style.css', 
-						array(),  // if the parent theme code has a dependency, copy it to here
-						$theme->parent()->get('Version')
-					);
-					
-					wp_enqueue_style( 'child-style', get_stylesheet_uri(),
-						array( $parenthandle ),
-						$theme->get('Version') // this only works if you have Version in the style header
-					);
-					
-				
-				//Striker child theme javascript js file
-					wp_enqueue_script('child-theme-js', get_stylesheet_directory_uri() . '/script.js', array( 'jquery' ), '1.0', true );
-					
-				}
-
-
-				/* CUSTOM PHP CODE GOES HERE */
+		//Striker Child Theme
+        function my_theme_enqueue_styles() {
+        
+         $parent_style = 'parent-style'; // Estos son los estilos del tema padre recogidos por el tema hijo.
+        
+         wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+         wp_enqueue_style( 'child-style',
+                 get_stylesheet_directory_uri() . '/style.css',
+                 array( $parent_style ),
+                 wp_get_theme()->get('Version')
+                 );
+        }
+        add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+		/* CUSTOM PHP CODE GOES HERE */
 ?>		
